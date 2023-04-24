cff204653c41fc6e76c27f49311705c72a43f50f
Will Lee <will@willlee.ca>
2023-04-24 13:37:41 -0600

    Styling and title update

--- Components/StartPage.jsx
+++ Components/StartPage.jsx
@@ -1,11 +1,19 @@
-const StartPage = ({buttonHandler}) => {
-    return (
-        <div className='startpage-container'>
-            <h1>Quizzical</h1>
-            <p>Feeling smart today? See how you do with this quick 5 question quiz. Start the quiz below!</p>
-            <button className="buttonStyle" onClick={buttonHandler}>Start quiz</button>
-        </div>           
-    )
-}
+const StartPage = ({ buttonHandler }) => {
+  return (
+    <div className="startpage-container">
+      <h1>Welcome to Stumped Ya!!</h1>
+      <p className="description">
+        Feeling smart today? Or do you just want to have some conversation
+        starters at your next hang out? Beef up your random knowledge with this
+        quick 5 question quiz. Hit start quiz below!
+      </p>
+      <button className="buttonStyle" onClick={buttonHandler}>
+        Start quiz
+      </button>
+      <p className="footer">Made using the 🧠 of Will Lee</p>
+      <p>© 2023 Will Lee Design</p>
+    </div>
+  );
+};
 
-export default StartPage
+export default StartPage;
